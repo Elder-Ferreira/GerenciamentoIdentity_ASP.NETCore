8ccfb1ae33f94f36d495effb43b79efb24ad1da0
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-07-07 18:10:41 -0300

    Criação do Fluxo de Registro

--- GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
+++ GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c141a0fcb2d31dd3902f0f514af769b3e864bac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6656ee353ead928e44ed6159046fb4f46e412f52")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
