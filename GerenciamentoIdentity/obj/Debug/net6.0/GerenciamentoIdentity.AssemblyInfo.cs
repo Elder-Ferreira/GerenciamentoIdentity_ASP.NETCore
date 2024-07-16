72db67502eda539430e9298e4a32672ca89b23e2
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-07-15 23:48:25 -0300

    Personalização da página Account/ManageNav

--- GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
+++ GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5bc791ad06e290305ce96e080bb3d2df4d9f40f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc46834fa4a9dc9eac657cec8376b1846abdbd8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
