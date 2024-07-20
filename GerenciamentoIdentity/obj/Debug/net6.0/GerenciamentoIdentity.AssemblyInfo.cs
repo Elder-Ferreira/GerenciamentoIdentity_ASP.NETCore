fd7a6a4c45258c578b47c5d1aa541a6e75a2766e
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-07-20 18:33:47 -0300

    Adicionado página Alteração de senha

--- GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
+++ GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527fe5d7b524c3efdd3d6c518033dc896a583ca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f43cd161ce4feb140005ee35b196b2f959a58e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
