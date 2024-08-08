aea75ebea8dda2c6029d5a12b7ca1cf03cd5af80
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-08-07 23:11:41 -0300

    Alteração da página de reenvio de email

--- GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
+++ GerenciamentoIdentity/obj/Debug/net6.0/GerenciamentoIdentity.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad5bf2c6a48d81eb742249ea6905955f0ce0143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93076510d80dab2fa013cccd46cc13342706a75f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
